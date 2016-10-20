d2a4077fa23f290bb07ec2a3797cb5d9c933bc43
Meelis Talvis <mtalvis@itcollege.ee>
2016-10-20 23:05:25 +0300

    Base for retrieving data from Spotify API

---
+++ discoverWeeklyScript.ps1
@@ -0,0 +1,11 @@
+﻿$url = "https://api.spotify.com/v1/users/spotifydiscover/playlists/6N7mRyylqso6ZQls1pS0Vp/tracks"
+$body = @{
+Accept="application/json";
+Authorization=" Bearer BQAecfU6h3utGO31jDBOualmVDSMQJA-xU2g3bTejiifinjx8nSSi3buhvgf6mRWoqs0cs7wTLagxsmniV78C3_QFfhn-8SmGlbKSncHusVrUthy83P6Bd6-zaOWhOrC2Dsd5NnhC4M"
+}
+
+$date = Get-Date -Format dd-MM-yyyy_HH-mm
+$filePath = "D:\Spotify\DiscoverWeekly\" + $date + ".xml"
+$filePath = "D:\Spotify\DiscoverWeekly\test.xml"
+$tracks = Invoke-RestMethod -Uri $url -Method Get -Headers $body
+$tracks | Export-Clixml -Path $filePath
